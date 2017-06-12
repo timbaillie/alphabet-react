7c280829c925175b0fc7e8881e1faec62c263ef1
Tim Baillie <timbaillieuk@gmail.com>
2017-06-12 09:30:30 +0100

    Add files via upload
    adding files from work laptop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+
+import App from './components/App';
+import About from './components/About';
+import NotFound from './components/NotFound';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const Root = () => {
+    return(
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={App}/>
+                    <Route path="/about" component={About}/>
+                    <Route component={NotFound}/>
+                </Switch>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+            </div>
+        </Router>
+    );
+}
+
+render(<Root/>, document.getElementById('root'));
